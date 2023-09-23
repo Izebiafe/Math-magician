fb1c1d3f04c8f117d2a96dc0225c4f488e7ed72b
izebiafe <afimiafidelis95@gmail.com>
2023-09-23 18:55:31 +0100

    fix linters

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -13,7 +13,5 @@ describe('calculate function', () => {
   it('should handle operation buttons', () => {
     const result1 = calculate({ next: '5' }, '+');
     expect(result1).toEqual({ total: '5', next: null, operation: '+' });
-
-
   });
-})
+});
